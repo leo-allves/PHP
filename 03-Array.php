f31aa306ad24393f91c9c540e6ad6b4689cb037c
Leonardo Alves <leonardoalves1992@outlook.com>
2020-11-30 16:29:56 -0300

    Aprendendo Array

--- 03-Array.php
+++ 03-Array.php
@@ -2,10 +2,23 @@
 
 //Lista de Bolos
 
-$ingredientes = ['açucar', 'ovo', 'leite', 'pó royal', 'massa'];
+$ingredientes = [
+    'açucar',
+    'ovo',
+    'leite',
+    'pó royal',
+    'massa'
+];
 
 //para mostrar 1 item
-echo $ingredientes[0];
+echo $ingredientes[2];
 
 //para mostrar todos os intens
 //print_r($ingredientes);
+
+/*
+$numerosDaMegaSena = [10, 14, 27, 34, 8];
+
+echo "Número 1: ".$numerosDaMegaSena[0]."<br/>";
+echo "Número 1: ".$numerosDaMegaSena[2];
+*/
