637988d3ce532fa5606431ff2a299e494f17039e
Leonardo Alves <leonardoalves1992@outlook.com>
2020-11-30 15:41:17 -0300

    Create exemplo de soma

---
+++ 1-soma-aritmetica.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "Soma aritimetica de notas".'<br/>';
+
+$primeiraNota = 10;
+$segundaNota = 9;
+$terceiraNota = 8;
+
+$soma = (($primeiraNota + $segundaNota + $terceiraNota) / 3);
+
+echo ("A soma dessa conta é: ".$soma." Párabens!");
