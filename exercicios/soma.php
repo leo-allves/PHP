48d6bd6d04879a058ee7a215dcba8cd7da8ace17
leo-allves <leonardoallves1992@gmail.com>
2020-12-01 08:52:50 -0300

    remoção de arquivo

--- exercicios/soma.php
+++ exercicios/soma.php
@@ -1,13 +1,26 @@
-<?php
-$nota1 = 8;
-$nota2 = 9;
-$nota3 = 0;
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cauculo Aritmético</title>
+</head>
+<body>
+    <?php
+        $nota1 = 8;
+        $nota2 = 9;
+        $nota3 = 0;
 
-$soma = (($nota1 + $nota2 + $nota3)/ 3);
+        $soma = (($nota1 + $nota2 + $nota3)/ 3);
 
-if ($soma <=6) {
-    echo "Você está de recupercão sua nota foi de: ".$soma;
-}
-else {
-    echo "Você foi aprovado parabéns sua nota foi de: ".$soma;
-}
+        if ($soma <=6) {
+            echo "Você está de recupercão sua nota foi de: ".$soma;
+        }
+        else {
+            echo "Você foi aprovado parabéns sua nota foi de: ".$soma;
+        }
+    ?>
+</body>
+</html>
+
+
