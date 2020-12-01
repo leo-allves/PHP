559370a86f2ada23699abcddf2460852eacd04f4
leo-allves <leonardoallves1992@gmail.com>
2020-12-01 08:47:46 -0300

    Adicionando Calculo Aritmético

---
+++ exercicios/soma.php
@@ -0,0 +1,13 @@
+<?php
+$nota1 = 8;
+$nota2 = 9;
+$nota3 = 0;
+
+$soma = (($nota1 + $nota2 + $nota3)/ 3);
+
+if ($soma <=6) {
+    echo "Você está de recupercão sua nota foi de: ".$soma;
+}
+else {
+    echo "Você foi aprovado parabéns sua nota foi de: ".$soma;
+}
